a9de2e6a3e097bc47e349446290e4db20b93dd81
unknown <geethujithink@gmail.com>
2023-05-28 18:01:03 -1200

    hooo

---
+++ travelproject/travelapp/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+
+
+
+def home(request):
+    return render(request,"index.html")
+def about(request):
+    return render(request,"about.html")
+def contact(request):
+    return render(request,"contact.html")
+def details(request):
+    return HttpResponse("details")
+def thanks(request):
+    return HttpResponse("thank you")
